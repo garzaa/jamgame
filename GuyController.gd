8fe2a42bf8865121bc0c60257060c9cca9699b3b
Adrian Garza <adrianhgarza@gmail.com>
2023-04-08 22:04:09 -0700

    ough

--- GuyController.gd
+++ GuyController.gd
@@ -6,14 +6,13 @@ var jump_speed = -400.0
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	velocity.y += gravity * delta
+    velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = jump_speed
+    if Input.is_action_just_pressed("Jump") and is_on_floor():
+        velocity.y = jump_speed
 
-	var direction = Input.get_axis("Left", "Right")
-	velocity.x = direction * speed
+    var direction = Input.get_axis("Left", "Right")
+    velocity.x = direction * speed
 
-	move_and_slide()
+    move_and_slide()
